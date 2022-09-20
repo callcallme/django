5d22a98ac0bef84700f48bf98cfd55fe2ff674be
CheWei <jokingone333@gmail.com>
2022-09-20 23:38:04 +0800

    first commit

---
+++ django/mysite/trips/views.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from django.shortcuts import render
+from .models import Post
+#from django.http import HttpResponse
+# Create your views here.
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+
+    
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
+    
+    
+    
+    
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post.html', {'post': post})
+
+
+
+
+    
